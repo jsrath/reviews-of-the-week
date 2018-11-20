3222b618d8aede4b075722a6f4187cd974392546
Jeremy <jeremy@jsrath.com>
2018-11-20 17:45:43 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 import './App.css';
 
 class App extends Component {
@@ -14,12 +15,27 @@ class App extends Component {
       .then(items => this.setState({ items }));
   }
 
+  printStars(number) {
+    return Array(Math.floor(number)).fill(<i className="fas fa-star" />);
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.items.map(item => (
-          <p>{item.videoTitle}</p>
-        ))}
+        {this.state.items
+          .sort((a, b) => b.dateReleased.localeCompare(a.dateReleased))
+          .map(
+            (item, index) =>
+              index < 6 && (
+                <div>
+                  <h2>
+                    {item.manufacturer} | {item.product}
+                  </h2>
+                  <p>Rating: {this.printStars(item.rating)}</p>
+                  <YouTube videoId={item.videoCode} />
+                </div>
+              ),
+          )}
       </div>
     );
   }
