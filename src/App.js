c05171be628d8798d45d7372d28a3f6d28ee0afe
Jeremy <jeremy@jsrath.com>
2018-11-21 20:33:01 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
 import React, { Component } from 'react';
-import YouTube from 'react-youtube';
+import New from './components/New/New';
+import Top from './components/Top/Top';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-    };
-  }
-  componentDidMount() {
-    fetch('https://thereportoftheweek-api.herokuapp.com/reports')
-      .then(response => response.json())
-      .then(items => this.setState({ items }));
-  }
-
-  printStars(number) {
-    return Array(Math.floor(number)).fill(<i className="fas fa-star" />);
-  }
-
   render() {
     return (
       <div className="App">
-        {this.state.items
-          .sort((a, b) => b.dateReleased.localeCompare(a.dateReleased))
-          .map(
-            (item, index) =>
-              index < 6 && (
-                <div>
-                  <h2>
-                    {item.manufacturer} | {item.product}
-                  </h2>
-                  <p>Rating: {this.printStars(item.rating)}</p>
-                  <YouTube videoId={item.videoCode} />
-                </div>
-              ),
-          )}
+        <New />
+        <Top />
       </div>
     );
   }
