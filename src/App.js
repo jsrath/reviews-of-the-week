1bbe57ec0d9ab2c7b479eb4aa04cb354afed6491
Jeremy <jeremy@jsrath.com>
2018-11-22 12:02:49 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
 import New from './components/New/New';
 import Top from './components/Top/Top';
 import './App.css';
@@ -7,8 +8,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <New />
-        <Top />
+        <Header />
+        <div className="top-section">
+          <hr />
+          <New />
+          <Top />
+        </div>
       </div>
     );
   }
